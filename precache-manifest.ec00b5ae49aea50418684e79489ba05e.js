534e338bc643a7b846ea997dc2bb4bdf317eba35
Ammar Ghazali <ammar_ghazali@yahoo.de>
2019-07-25 11:08:05 +0800

    Updates

---
+++ precache-manifest.ec00b5ae49aea50418684e79489ba05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5f0297a428f6273aa1eec2b1ea20a1",
+    "url": "/ammarghazali/contactmanager/index.html"
+  },
+  {
+    "revision": "575dccf0629d12cf3b99",
+    "url": "/ammarghazali/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c15a6ff69af8b5848918",
+    "url": "/ammarghazali/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "575dccf0629d12cf3b99",
+    "url": "/ammarghazali/contactmanager/static/js/2.c48b6df6.chunk.js"
+  },
+  {
+    "revision": "c15a6ff69af8b5848918",
+    "url": "/ammarghazali/contactmanager/static/js/main.91f1d979.chunk.js"
+  },
+  {
+    "revision": "8f73c71377f31a29846e",
+    "url": "/ammarghazali/contactmanager/static/js/runtime~main.208ee44a.js"
+  }
+]);
